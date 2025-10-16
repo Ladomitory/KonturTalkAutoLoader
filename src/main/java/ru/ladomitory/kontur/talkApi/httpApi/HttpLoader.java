b0e5eb653fb5515280df0991b0c3385acfd2df45
RHolmogorov <holmogorov.r@novosoft.net>
2025-10-16 19:15:02 +0700

    Add load system

---
+++ src/main/java/ru/ladomitory/kontur/talkApi/httpApi/HttpLoader.java
@@ -0,0 +1,50 @@
+package ru.ladomitory.kontur.talkApi.httpApi;
+
+import ru.ladomitory.kontur.talkApi.util.JSONMap;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class HttpLoader {
+    private final HttpClient CLIENT;
+    private final String DOMAIN;
+    private final String AUTH_TOKEN;
+
+    public HttpLoader(String domain, String authToken) {
+        CLIENT = HttpClient.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .build();
+        DOMAIN = domain;
+        AUTH_TOKEN = authToken;
+    }
+
+    protected HttpResponse<String> createGetRequest(String path) {
+        return createGetRequest(path, new JSONMap());
+    }
+
+    protected HttpResponse<String> createGetRequest(String path, JSONMap params) {
+        String paramsString = params.toJSONString();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .version(HttpClient.Version.HTTP_2)
+                .uri(URI.create(DOMAIN + path))
+                .header("X-Auth-Token", AUTH_TOKEN)
+                .timeout(Duration.ofMinutes(5))
+                .method("GET", HttpRequest.BodyPublishers.ofString(paramsString))
+                .build();
+        HttpResponse<String> response;
+        try {
+            response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        return response;
+    }
+}
---
+++ src/main/java/ru/ladomitory/kontur/talkApi/kontur/KonturTalkLoader.java
@@ -0,0 +1,74 @@
+package ru.ladomitory.kontur.talkApi.kontur;
+
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import ru.ladomitory.kontur.talkApi.httpApi.HttpLoader;
+import ru.ladomitory.kontur.talkApi.util.JSONMap;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+import static ru.ladomitory.kontur.talkApi.util.PropertiesReader.getStringProperty;
+
+public class KonturTalkLoader extends HttpLoader {
+    private static final Logger logger = LogManager.getLogger(KonturTalkLoader.class);
+
+    private static final String PROTOCOL = getStringProperty("loader.protocol");
+    private static final String DOMAIN_NAME = getStringProperty("loader.domainName");
+    private static final String CONFERENCE_HISTORY_PATH = getStringProperty("loader.conferenceHistory.path");
+    private static final String CONFERENCE_REPORTS_PATH = getStringProperty("loader.conferenceReport.path");
+    private static final String PARTICIPANTS_PATH = getStringProperty("loader.conferenceReport.participants.path");
+
+    public KonturTalkLoader(String space, String authToken) {
+        super(PROTOCOL + space + DOMAIN_NAME, authToken);
+    }
+
+    public String getConferenceHistory() {
+        return getConferenceHistory(null, null, 0, 100, null);
+    }
+
+    public String getConferenceHistory(String fromDate, String toDate, int skip, int take, List<String> roomNames) {
+        JSONMap params = new JSONMap();
+        if (fromDate != null) {
+            params.put("fromDate", "\"" + fromDate + "\"");
+        }
+        if (toDate != null) {
+            params.put("toDate", "\"" + toDate + "\"");
+        }
+        if (skip > 0) {
+            params.put("\"skip\"", Integer.toString(skip));
+        }
+        if (take != 100) {
+            params.put("\"take\"", Integer.toString(take));
+        }
+        if (roomNames != null && !roomNames.isEmpty()) {
+            String array = "[\n";
+            for (String roomName : roomNames) {
+                array += "\"" + roomName + "\",\n";
+            }
+            array += "]";
+            params.put("\"roomName\"", array);
+        }
+        HttpResponse<String> response = createGetRequest(CONFERENCE_HISTORY_PATH, params);
+        if (response.statusCode() == 200) {
+            System.out.println("Response: Status=" + response.statusCode());
+            return response.body();
+        } else {
+            logger.log(Level.INFO, "Response: Status=" + response.statusCode());
+            return null;
+        }
+    }
+
+    public String getParticipantsReport(String conferenceKey) {
+        HttpResponse<String> response = createGetRequest(CONFERENCE_REPORTS_PATH + conferenceKey + PARTICIPANTS_PATH);
+        if (response.statusCode() == 200) {
+            System.out.println("Response: Status=" + response.statusCode());
+            return response.body();
+        } else {
+            logger.log(Level.INFO, "Response: Status=" + response.statusCode());
+            return null;
+        }
+    }
+}
